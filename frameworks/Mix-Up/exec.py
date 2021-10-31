8439c41bb928068137be1e1274f9d6844f800ce6
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-10-30 20:10:04 -0700

    fixed up mix-up not to convex combine categoricals

--- frameworks/Mix-Up/exec.py
+++ frameworks/Mix-Up/exec.py
@@ -57,31 +57,42 @@ def run(dataset, config):
     if problem_type is REGRESSION:
         train_data = TabularDataset(train)
         test_data = TabularDataset(test)
-        predictor = TabularPredictor(label=label).fit(train_data, time_limit=30)
+        predictor = TabularPredictor(label=label).fit(train_data, time_limit=10)
         X, y, X_val, y_val, X_unlabeled, holdout_frac, num_bag_folds, groups = predictor._learner.general_data_processing(
             train_data, None, test_data, 0, 1)
 
         train_data = X.copy()
         y = y.reset_index(drop=True)
         train_data[label] = y
-        test = X_unlabeled.copy()
 
-        category_features = train_data.dtypes == 'category'
-        categorical_features = train_data.columns[category_features]
+        numerical_features = train_data.columns[train_data.dtypes != 'category']
+        categorical_features = train_data.columns[train_data.dtypes == 'category']
 
-        for feat in categorical_features:
-            train_data[feat] = pd.to_numeric(train_data[feat])
+        train_data_mixed = None
 
-        sample_sz = int(len(train_data) / 2)
-        lam = np.random.beta(0.4, 0.4, size=sample_sz)
-        lam = np.repeat(lam[:, None], len(train_data.columns), axis=1)
-        sample_1 = train_data.sample(n=sample_sz).reset_index(drop=True)
-        sample_2 = train_data.sample(n=sample_sz).reset_index(drop=True)
+        for index, row in train_data.iterrows():
+            train_data_subset = train_data.drop(index=index, axis='rows')
+            row_equal_cate = (row[categorical_features] == train_data_subset[categorical_features]).all(
+                axis='columns').copy()
+            row_eq_true = row_equal_cate[row_equal_cate]
 
-        sample_1_lam = lam * sample_1
-        sample_2_lam = (1 - lam) * sample_2
-        X_mix_up = sample_1_lam + sample_2_lam
-        train = train_data.append(X_mix_up, ignore_index=True).reset_index(drop=True)
+            if row_eq_true.empty:
+                continue
+            else:
+                lam = np.random.beta(0.4, 0.4)
+                sampled_idx = row_eq_true.sample(1)
+                sampled_row = train_data_subset.loc[sampled_idx.index]
+
+                x_curr_mixed = lam * sampled_row[numerical_features] + (1 - lam) * row[numerical_features]
+                x_curr_mixed[categorical_features] = row[categorical_features]
+
+                if train_data_mixed is not None:
+                    train_data_mixed = train_data_mixed.append(x_curr_mixed, ignore_index=True)
+                else:
+                    train_data_mixed = x_curr_mixed
+
+        train = train_data.append(train_data_mixed)
+        train = train.reset_index(drop=True)
 
     models_dir = tempfile.mkdtemp() + os.sep  # passed to AG
 
